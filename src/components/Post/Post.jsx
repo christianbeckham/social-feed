c1419e55eb102ec712ffc517b30d261b5440b84f
Christian Beckham <urenacb22@gmail.com>
2022-10-20 13:41:55 -0500

    feat: updated Post component with state to track updated values and edit mode, added functions to handle change, cancel and update events, and updated JSX to include update buttons and input

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import PostActions from "../PostActions/PostActions";
 
 const Post = (props) => {
-	const { post } = props;
+	const { post, updatePost } = props;
+	const [editMode, setEditMode] = useState(false);
+	const [updates, setUpdates] = useState({});
+
+	const toggleEditMode = () => setEditMode((prevState) => !prevState);
+
+	const handleUpdateChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setUpdates((prevState) => ({ ...prevState, [name]: value }));
+	};
+
+	const handleUpdateClick = (e) => {
+		const modifiedDate = new Date().toLocaleDateString();
+		const updatedPostValues = {
+			...updates,
+			modifiedDate,
+		};
+		updatePost(post.id, updatedPostValues);
+		toggleEditMode();
+		setUpdates({});
+	};
+
+	const handleCancelClick = (e) => {
+		toggleEditMode();
+		setUpdates({});
+	};
 
 	return (
 		<li className="list-group-item py-3 px-5">
@@ -10,11 +36,49 @@ const Post = (props) => {
 				<span>{post.username}</span>
 				{" -"}
 				<span className="badge font-monospace fw-lighter text-muted align-middle">
-					{post.createdDate}
+					{post.createdDate}{" "}
+					{post.hasOwnProperty("modifiedDate") ? (
+						<span className="fst-italic">(Edited)</span>
+					) : null}
 				</span>
 			</div>
-			<p className="fw-normal">{post.postMessage}</p>
-			<PostActions post={post} />
+			{editMode === false ? (
+				<p className="fw-normal">{post.postMessage}</p>
+			) : (
+				<React.Fragment>
+					<textarea
+						className="form-control my-2"
+						name="postMessage"
+						value={updates.postMessage || post.postMessage}
+						onChange={handleUpdateChange}
+					></textarea>
+					<div
+						className="btn-group btn-group-sm"
+						role="group"
+						aria-label="Small button group"
+					>
+						<button
+							type="button"
+							className="btn btn-success"
+							onClick={handleUpdateClick}
+						>
+							Update
+						</button>
+						<button
+							type="button"
+							className="btn btn-danger"
+							onClick={handleCancelClick}
+						>
+							Cancel
+						</button>
+					</div>
+				</React.Fragment>
+			)}
+			<PostActions
+				post={post}
+				toggleEditMode={toggleEditMode}
+				updatePost={updatePost}
+			/>
 		</li>
 	);
 };
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Post from "../Post/Post";
 
 const PostList = (props) => {
-	const { posts } = props;
+	const { posts, updatePost } = props;
 
 	return (
 		<div className="mb-5">
 			<ul className="list-group rounded-4 shadow">
 				{posts.map((post) => (
-					<Post key={post.id} post={post} />
+					<Post key={post.id} post={post} updatePost={updatePost} />
 				))}
 			</ul>
 		</div>
